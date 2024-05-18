ddd78b5a04f33b1115449c1772a5f263d0a1d009
Ehtesham1234 <ehteshamusman12344321@gmail.com>
2024-05-18 17:26:47 +0530

    changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,7 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/lists").post(list);
-router.route("/lists/:listId/users").post(upload.single("file"), users);
+router.route("/lists/users/:listId").post(upload.single("file"), users);
 router.get("/download/:filename", function (req, res) {
   const filename = req.params.filename;
   const file = `./${filename}`; // If the file is in the outermost directory
